dc6a9b602e375307dc2f9ae33fdf283f348cc4ff
Adam Jordanek <dotevo@gmail.com>
2018-05-14 23:04:21 +0200

    New migration

--- install/migrations/migration_8.sql
+++ install/migrations/migration_8.sql
@@ -1,24 +1,15 @@
 ALTER TABLE %PREFIX%trades
-ADD COLUMN `planet_id` INT(11) NULL DEFAULT NULL AFTER `ex_resource_amount`;
-
-ALTER TABLE %PREFIX%trades
-ADD COLUMN `trade_fleet_array` TEXT NULL DEFAULT NULL ;
-
-ALTER TABLE %PREFIX%trades
-CHANGE COLUMN `seller_fleet_id` `seller_fleet_id` BIGINT(11) UNSIGNED NULL DEFAULT '0' ,
+ADD COLUMN `trade_id` INT NOT NULL AUTO_INCREMENT,
 DROP PRIMARY KEY,
-ADD PRIMARY KEY (`trade_id`);
-
-
-ALTER TABLE %PREFIX%trades
-ADD COLUMN `trade_id` INT NOT NULL AUTO_INCREMENT AFTER `trade_fleet_array`,
-DROP PRIMARY KEY,
-ADD PRIMARY KEY (`seller_fleet_id`, `trade_id`),
+ADD PRIMARY KEY (`trade_id`),
 ADD UNIQUE INDEX `trade_id_UNIQUE` (`trade_id` ASC);
 
-
 ALTER TABLE %PREFIX%trades
-ADD COLUMN `resource_metal` DOUBLE(50,0) NULL AFTER `trade_id`,
-ADD COLUMN `resource_crystal` DOUBLE(50,0) NULL AFTER `resource_metal`,
-ADD COLUMN `resource_deuterium` DOUBLE(50,0) NULL AFTER `resource_crystal`,
-ADD COLUMN  `closed` TINYINT(4) NULL AFTER `resource_deuterium`;
+ADD COLUMN `planet_id` INT(11) NULL DEFAULT NULL,
+ADD COLUMN `trade_fleet_array` TEXT NULL DEFAULT NULL,
+CHANGE COLUMN `seller_fleet_id` `seller_fleet_id` BIGINT(11) UNSIGNED NULL DEFAULT '0' ,
+ADD COLUMN `resource_metal` DOUBLE(50,0) NULL,
+ADD COLUMN `resource_crystal` DOUBLE(50,0) NULL,
+ADD COLUMN `resource_deuterium` DOUBLE(50,0) NULL,
+ADD COLUMN  `closed` TINYINT(4) NULL DEFAULT '0',
+DROP PRIMARY KEY;
