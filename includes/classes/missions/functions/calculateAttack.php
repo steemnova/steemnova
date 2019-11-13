fa26a512ec970abf9445673c6e13a31c5ac58ebc
fervi <lordfervi@protonmail.com>
2019-11-13 12:50:17 +0100

    Added a hybrid battle engine

--- includes/classes/missions/functions/calculateAttack.php
+++ includes/classes/missions/functions/calculateAttack.php
@@ -1,10 +1,33 @@
 <?php
 
 /* Battle engines
+
 0 - Ogame probabilistic battle engine (Fast, stable, but buggy)
-1 - SteemNova; Slow, unstable on big battles, required custom PHP Extensions)
+1 - SteemNova; Slow, unstable on big battles, required custom PHP Extension)
+2 - Hybrid; Below 200000 ships SteemNova, Above OPBE
+999 - SteemNova's Battle Engine based on Arrays; Very slow, not recommended
 */
 
-$battle_engine = 1;
+$battle_engine = 2;
 
-if($battle_engine==1) { include("SteemNova.php"); } else { include("OPBE.php"); }
+/*
+DON'T MODIFY ↓
+*/
+
+if($battle_engine == 2) {
+$ships = 0;
+foreach($attackers[0]["unit"] as $units) {
+$ships = $ships + $units;
+}
+
+foreach($defenders[0]["unit"] as $units) {
+$ships = $ships + $units;
+}
+
+if($ships<=200000 AND extension_loaded('ds')) {
+include("SteemNova.php"); }
+else if($ships<=20000 AND !extension_loaded('ds')) {
+include("SteemNova_Array.php"); }
+else { include("OPBE.php"); }
+
+} else if($battle_engine == 1) {include("SteemNova.php");} else if($battle_engine == 999) {include("SteemNova_Array.php");} else {include("OPBE.php");}
