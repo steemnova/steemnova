ccb151074efba59fc4d775c5e2820c9e007f8ca3
fervi <lordfervi@protonmail.com>
2019-11-17 21:14:39 +0100

    Event added: Fleet Upgrade for 24h

--- external/events.php
+++ external/events.php
@@ -4,7 +4,7 @@ $config = file("config.txt");
 $connection = mysqli_connect(trim($config[0]), trim($config[1]), trim($config[2]), trim($config[3]));
 $database = mysqli_fetch_all(mysqli_query($connection, "SELECT id FROM uni1_users"));
 
-$random_event = rand(1,2);
+$random_event = rand(1,3);
 $time = time();
 $nickname = '<span class="admin">SteemNova Event System</span>';
 $subject = '<span class="admin">New Event</span>';
@@ -27,6 +27,15 @@ if($random_event==1) {
 	mysqli_query($connection, "UPDATE uni1_users SET dm_energie=($time+86400) WHERE id=$id");
 	} else {
 	mysqli_query($connection, "UPDATE uni1_users SET dm_energie=dm_energie+86400 WHERE id=$id");
+	}
+} else if($random_event==3) {
+	$text = '<span class="admin">We found the coal reserves! Thanks to it, our ships will be 10% faster! Unfortunately, we do not have too many stocks of this resource ...</span>';
+	mysqli_query($connection, "INSERT INTO uni1_messages (message_owner, message_sender, message_time, message_type, message_from, message_subject, message_text, message_unread, message_universe) VALUES ($id, 1, $time, 50, '$nickname', '$subject', '$text', 1, 1)");
+	$dm = mysqli_fetch_all(mysqli_query($connection, "SELECT dm_fleettime FROM uni1_users WHERE id=$id"));
+	if($dm[0][0]==0) {
+	mysqli_query($connection, "UPDATE uni1_users SET dm_fleettime=($time+86400) WHERE id=$id");
+	} else {
+	mysqli_query($connection, "UPDATE uni1_users SET dm_fleettime=dm_fleettime+86400 WHERE id=$id");
 	}
 }
 
