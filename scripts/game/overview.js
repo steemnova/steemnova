95847c4fa2a9b83a6e78f100821592c33aec8bde
Bastian Luettig <bastie@bastie.org>
2021-08-28 00:03:21 +0200

    fixes timeoverflow in overview

--- scripts/game/overview.js
+++ scripts/game/overview.js
@@ -1,24 +1,30 @@
-$(document).ready(function()
-{
-	window.setInterval(function() {
-		$('.fleets').each(function() {
-			var s		= $(this).data('fleet-time') - (serverTime.getTime() - startTime) / 1000;
-			if(s <= 0) {
-				$(this).text('-');
-			} else {
-				$(this).text(GetRestTimeFormat(s));
-			}
-		})
-	}, 1000);
-	
-	window.setInterval(function() {
-		$('.timer').each(function() {
-			var s		= $(this).data('time') - (serverTime.getTime() - startTime) / 1000;
-			if(s == 0) {
-				window.location.href = "game.php?page=overview";
-			} else {
-				$(this).text(GetRestTimeFormat(s));
-			}
-		});
-	}, 1000);
+function fleetCountDown() {
+    $('.fleets').each(function () {
+        var s = $(this).data('fleet-time') - (serverTime.getTime() - startTime) / 1000;
+        if (s <= 0) {
+            $(this).text('-');
+        } else {
+            $(this).text(GetRestTimeFormat(s));
+            window.setTimeout(fleetCountDown, 1000);
+        }
+    })
+}
+
+function timeCountDown() {
+    $('.timer').each(function () {
+        var s = $(this).data('time') - (serverTime.getTime() - startTime) / 1000;
+        if (s == 0) {
+            window.location.href = "game.php?page=overview";
+        } else {
+            $(this).text(GetRestTimeFormat(s));
+            window.setTimeout(timeCountDown, 1000);
+        }
+    });
+}
+
+$(document).ready(function () {
+    window.setTimeout(function () {
+        fleetCountDown();
+        timeCountDown();
+    }, 1000);
 });
