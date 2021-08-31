ece5a76e7ce5151fbf80804fdf02d8fff391db58
Bastian Luettig <bastie@bastie.org>
2021-08-31 13:49:16 +0200

    simplified ref earnings

--- includes/classes/class.Cuneros.php
+++ includes/classes/class.Cuneros.php
@@ -30,17 +30,14 @@ class Cuneros
 
         if($targetId === NULL) {
             $sol_id = $USER['ref_id'];
-            $USER['ref_coins'] += $refEarn;
         } else {
             $sql = 'SELECT ref_id FROM %%USERS%% WHERE id = :userId';
             $sol_id = Database::get()->selectSingle($sql, ['userId' => $targetId], 'ref_id');
-            if($sol_id) {
-                $sql = 'UPDATE %%USERS%% SET coins ref_coint = ref_coins + :Amount WHERE id = :userId';
-                Database::get()->update($sql, ['Amount' => $refEarn, 'userId' => $targetId]);
-            }
         }
         if ($refEarn > 0 && $sol_id != 0) { // user has werber
             Cuneros::claimCuneros($refEarn, $sol_id, false);
+            $sql = 'UPDATE %%USERS%% SET coins ref_coint = ref_coins + :Amount WHERE id = :userId';
+            Database::get()->update($sql, ['Amount' => $refEarn, 'userId' => $targetId]);
         }
     }
 }
