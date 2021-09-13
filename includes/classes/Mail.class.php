c7875e053d6470b945385a1a19688e8442fe9717
Bastian Luettig <bastian.luettig@bastie.space>
2021-09-13 18:19:49 +0200

    fixes mail admin

--- includes/classes/Mail.class.php
+++ includes/classes/Mail.class.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- *  2Moons 
+ *  2Moons
  *   by Jan-Otto Kröpke 2009-2016
  *
  * For the full copyright and license information, please view the LICENSE
@@ -16,68 +16,66 @@
  */
 
 class Mail
-{	
-	static public function send($mailTarget, $mailTargetName, $mailSubject, $mailContent)
-	{		
-		$mail	= self::getMailObject();
-			 
-        $mail->CharSet	= 'UTF-8';
-        $mail->Subject	= $mailSubject;
-        $mail->Body		= $mailContent;
+{
+    static public function send($mailTarget, $mailTargetName, $mailSubject, $mailContent)
+    {
+        $mail = self::getMailObject();
+
+        $mail->CharSet = 'UTF-8';
+        $mail->Subject = $mailSubject;
+        $mail->Body = $mailContent;
         $mail->AddAddress($mailTarget, $mailTargetName);
-        $mail->Send(); 
-	}
+        $mail->Send();
+    }
 
-	static public function multiSend($mailTargets, $mailSubject, $mailContent = NULL)
-	{
-		$mail	= self::getMailObject();
+    static public function multiSend($mailTargets, $mailSubject, $mailContent = NULL)
+    {
+        $mail = self::getMailObject();
 
-		$mail->Subject	= $mailSubject;
+        $mail->Subject = $mailSubject;
 
-		foreach($mailTargets as $address => $data)
-		{
-			$content = isset($data['body']) ? $data['body'] : $mailContent;
-			
-			$mail->AddAddress($address, $data['username']);
-			$mail->MsgHTML($content);
-			$mail->Send(); 
-			$mail->ClearAddresses();
-		}
-	}
+        foreach ($mailTargets as $address => $data) {
+            $content = isset($data['body']) ? $data['body'] : $mailContent;
 
-	static private function getMailObject()
-	{
+            $mail->AddAddress($address, $data['username']);
+            $mail->MsgHTML($content);
+            $mail->Send();
+            $mail->ClearAddresses();
+        }
+    }
+
+    static private function getMailObject()
+    {
         require_once 'includes/libs/phpmailer/class.phpmailer.php';
 
-        $mail               = new PHPMailer(true);
-		$mail->PluginDir	= 'includes/libs/phpmailer/';
+        $mail = new PHPMailer(true);
+        $mail->PluginDir = 'includes/libs/phpmailer/';
 
-		$config				= Config::get();
+        $config = Config::get();
 
-        if($config->mail_use == 2) {
-			$mail->IsSMTP();  
-			$mail->SMTPSecure       = $config->smtp_ssl;                                            
-			$mail->Host             = $config->smtp_host;
-			$mail->Port             = $config->smtp_port;
-			
-			if($config->smtp_user != '')
-			{
-				$mail->SMTPAuth         = true; 
-				$mail->Username         = $config->smtp_user;
-				$mail->Password         = $config->smtp_pass;
-			}
-        } elseif($config->mail_use == 0) {
-			$mail->IsMail();
+        if ($config->mail_use == 2) {
+            $mail->IsSMTP();
+            $mail->SMTPSecure = $config->smtp_ssl;
+            $mail->Host = $config->smtp_host;
+            $mail->Port = $config->smtp_port;
+
+            if ($config->smtp_user != '') {
+                $mail->SMTPAuth = true;
+                $mail->Username = $config->smtp_user;
+                $mail->Password = $config->smtp_pass;
+            }
+        } elseif ($config->mail_use == 0) {
+            $mail->IsMail();
         } else {
-			throw new Exception("sendmail is deprecated, use SMTP instead!");
-		}
+            throw new Exception("sendmail is deprecated, use SMTP instead!");
+        }
 
-		$mailFromAddress	= $config->smtp_sendmail;
-		$mailFromName		= $config->game_name;
+        $mailFromAddress = $config->smtp_sendmail;
+        $mailFromName = $config->game_name;
+        $mail->Timeout = 15;
+        $mail->CharSet = 'UTF-8';
+        $mail->SetFrom($mailFromAddress, $mailFromName);
 
-		$mail->CharSet	= 'UTF-8';
-		$mail->SetFrom($mailFromAddress, $mailFromName);
-		
-		return $mail;
-	}
+        return $mail;
+    }
 }
--- styles/templates/adm/ConfigBasicBody.tpl
+++ styles/templates/adm/ConfigBasicBody.tpl
@@ -1,5 +1,4 @@
 {include file="overall_header.tpl"}
-<center>
 <form action="" method="post">
 <input type="hidden" name="opt_save" value="1">
 <table width="70%" cellpadding="2" cellspacing="2">
@@ -119,5 +118,4 @@
 </tr>
 </table>
 </form>
-</center>
 {include file="overall_footer.tpl"}
